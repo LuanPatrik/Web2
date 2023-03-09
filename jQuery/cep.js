cf702175fba0e5cd01b4ade37130a6bcc793d99a
LuanPatrik <72170325+LuanPatrik@users.noreply.github.com>
2023-03-08 23:54:41 -0300

    Atualização

---
+++ jQuery/cep.js
@@ -0,0 +1,21 @@
+$(()=>{
+    $('#btnSalvar').on('click', ()=>{
+        var numCep = $('#cep').val();
+        var url = "https://viacep.com.br/ws/"+numCep+"/json/";
+
+        $.ajax({
+            url: url,
+            type: "get",
+            dataType: "json",
+
+            success:function(dados){
+                console.log(dados);
+                $("#uf").val(dados.uf);
+                $("#cidade").val(dados.localidade);
+                $("#logradouro").val(dados.logradouro);
+                $("#bairro").val(dados.bairro);
+            }
+        })
+    });
+
+})
